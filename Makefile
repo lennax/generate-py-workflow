654fbea624bb3153a6904ad63371754c5b02f5b2
Lenna Peterson <lenna.peterson@gmail.com>
2025-04-05 12:35:41 -0400

    boilerplate

---
+++ Makefile
@@ -0,0 +1,11 @@
+.PHONY: create_venv test
+
+create_venv:
+	python3 -m venv .venv
+	./.venv/bin/python3 -m pip install --upgrade pip
+	./.venv/bin/python3 -m pip install -r requirements.txt
+	go install github.com/rhysd/actionlint/cmd/actionlint@latest
+
+test:
+	./.venv/bin/python3 generate_reusable_workflow.py example_script.py
+	actionlint workflow.yml
--- example_script.py
+++ example_script.py
@@ -1,10 +1,14 @@
 import argparse
 
-def main():
+def get_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument("filename", help="Filename to frobnicate")
     parser.add_argument("--dry-run", action="store_true")
     parser.add_argument("--count", type=int, help="Number of times to frobnicate")
+    return parser
+
+def main():
+    parser = get_parser()
     args = parser.parse_args()
     print(args)
 
